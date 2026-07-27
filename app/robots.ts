f2c85bac9be8c06c4de7493474c6fad9a2e0c8da
saif91alwasty-design <saif91alwasty@gmail.com>
2026-07-27 19:11:58 +0300

    Update robots.txt rules for user agents

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,25 @@
 import { MetadataRoute } from 'next';
-import { languages } from '@/lib/languages';
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://bg-remover99.vercel.app';
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/admin/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
